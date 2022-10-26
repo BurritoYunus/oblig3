bd450bc31d2164356b02cd13b00a9470c592a0f5
BurritoYunus <yunu22356@Hotmail.com>
2022-10-26 12:36:32 +0200

    lagt til oblig 2 filer

---
+++ SEG/src/test/java/leapyear_test.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class leapyear_test {
+
+    @Test
+    public void dividable_by_4_not_100()
+    {
+        assertEquals(true, leapyear.isLeapYear(1920));
+    }
+
+    @Test
+    public void not_dividable_by_4()
+    {
+        assertEquals(false, leapyear.isLeapYear(1567));
+    }
+
+    @Test
+    public void dividable_by_100()
+    {
+        assertEquals(true, leapyear.isLeapYear(2000));
+    }
+    @Test
+    public void not_dividable_by_100()
+    {
+        assertEquals(false, leapyear.isLeapYear(2200));
+    }
+}
